9bedafe7509c457b9af3017c936f0215cae995e6
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-08-25 15:09:17 -0400

    add getHumanChoice function

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ function getComputerChoice() {
   }
 }
 
+function getHumanChoice() {
+  let userChoice = prompt("Enter; Rock, Paper, or Scissors");
+  return userChoice;
+}
+
 /*
 Math.random() generates a decimal between 0 (inclusive) and 1 (exclusive).
 Math.random() * 3 generates a decimal between 0 and 3 (but less than 3).
 Math.floor(Math.random() * 3) converts this into an integer 0, 1, or 2.
 Adding 1 shifts the range to 1, 2, or 3, which corresponds to "Rock," "Paper," or "Scissors."
 */
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
