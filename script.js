304db437904e244e0194bb06b6f1e9a86073d6bf
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-17 20:26:31 -0400

    Add playGame function
    Created a function to call playRound multiple times to have multiple rounds and determine a winner.

--- script.js
+++ script.js
@@ -9,8 +9,6 @@
 5. Write logic to play an entire game that consists of 5 rounds. Declare a winner at the end
 
 */
-let playerScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
   const choiceArr = ["rock", "paper", "scissors"]; // Array holding choices from index 0 to 2
@@ -21,38 +19,69 @@ function getHumanChoice() {
   const humanChoice = prompt("Rock, paper, or scissors?"); // Prompts user to enter a choice
   return humanChoice.toLowerCase(); // returns the choice as a string to lower case
 }
-function playRound(humanChoice, computerChoice) {
-  switch (humanChoice) {
-    case "rock":
-      if (computerChoice === "scissors") {
-        console.log(`You win ${humanChoice} beats ${computerChoice}!`);
-      } else if (computerChoice === "paper") {
-        console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
-      } else {
-        console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
-      }
-      break;
-    case "scissors":
-      if (computerChoice === "paper") {
-        console.log(`You win ${humanChoice} beats ${computerChoice}!`);
-      } else if (computerChoice === "rock") {
-        console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
-      } else {
-        console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
-      }
-      break;
-    case "paper":
-      if (computerChoice === "rock") {
-        console.log(`You win ${humanChoice} beats ${computerChoice}!`);
-      } else if (computerChoice === "scissors") {
-        console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
-      } else {
-        console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
-      }
+
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    // switch goes through 3 possible human choices and compares the choice to the computer choice using if else and else if statements, checks for winning case first, then losing case, then tie case
+
+    switch (humanChoice) {
+      case "rock":
+        if (computerChoice === "scissors") {
+          console.log(`You win ${humanChoice} beats ${computerChoice}!`);
+          playerScore++;
+        } else if (computerChoice === "paper") {
+          console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+          computerScore++;
+        } else {
+          console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
+        }
+        break;
+      case "scissors":
+        if (computerChoice === "paper") {
+          console.log(`You win ${humanChoice} beats ${computerChoice}!`);
+          playerScore++;
+        } else if (computerChoice === "rock") {
+          console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+          computerScore++;
+        } else {
+          console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
+        }
+        break;
+      case "paper":
+        if (computerChoice === "rock") {
+          console.log(`You win ${humanChoice} beats ${computerChoice}!`);
+          playerScore++;
+        } else if (computerChoice === "scissors") {
+          console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+          computerScore++;
+        } else {
+          console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
+        }
+    }
   }
+
+  for (let i = 0; i < 5; i++) {
+    // gets the two choices human / computer (re called next loop)
+
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+    // plays one round with current choices
+    playRound(humanChoice, computerChoice);
+  }
+
+  const winningMessage =
+    playerScore > computerScore
+      ? `Congratulations you win! 
+      Player Score: ${playerScore}
+      Computer Score: ${computerScore}`
+      : `Womp, Womp, you lose!
+      Player Score: ${playerScore}
+      Computer Score: ${computerScore}`;
+  return winningMessage;
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-console.log(humanChoice, computerChoice);
-playRound(humanChoice, computerChoice);
+console.log(playGame());
