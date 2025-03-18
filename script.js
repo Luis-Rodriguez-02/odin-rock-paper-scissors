8af3d4cd134ba983b6beaf4b36eb026c400224d1
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-17 20:07:04 -0400

    Add playRound function
    Added function to play round with computer choice and player choice, contains logic required to determine winner

--- script.js
+++ script.js
@@ -9,16 +9,50 @@
 5. Write logic to play an entire game that consists of 5 rounds. Declare a winner at the end
 
 */
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   const choiceArr = ["rock", "paper", "scissors"]; // Array holding choices from index 0 to 2
   const randNum = Math.trunc(Math.random() * 3); // Generates random number from 0 to 3 not including 3
   return choiceArr[randNum]; // returns choice using random number as index.
 }
-
 function getHumanChoice() {
   const humanChoice = prompt("Rock, paper, or scissors?"); // Prompts user to enter a choice
-  return humanChoice; // returns the choice as a string
+  return humanChoice.toLowerCase(); // returns the choice as a string to lower case
+}
+function playRound(humanChoice, computerChoice) {
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice === "scissors") {
+        console.log(`You win ${humanChoice} beats ${computerChoice}!`);
+      } else if (computerChoice === "paper") {
+        console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+      } else {
+        console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
+      }
+      break;
+    case "scissors":
+      if (computerChoice === "paper") {
+        console.log(`You win ${humanChoice} beats ${computerChoice}!`);
+      } else if (computerChoice === "rock") {
+        console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+      } else {
+        console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
+      }
+      break;
+    case "paper":
+      if (computerChoice === "rock") {
+        console.log(`You win ${humanChoice} beats ${computerChoice}!`);
+      } else if (computerChoice === "scissors") {
+        console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+      } else {
+        console.log(`Its a tie! ${humanChoice} ties with ${computerChoice}`);
+      }
+  }
 }
 
-console.log(getHumanChoice());
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+console.log(humanChoice, computerChoice);
+playRound(humanChoice, computerChoice);
